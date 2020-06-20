a6f0c7942fa6fc2638d4b42126c92c8f1d6fe14b
joao.raminelli <joao.raminelli.ext@sascar.com.br>
2020-06-20 20:31:09 -0300

    adicionado as classes de repository e seus respectivos testes unitarios

---
+++ src/main/java/com/raminelli/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,10 @@
+package com.raminelli.pontointeligente.api.enums;
+
+public enum TipoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOÇO,
+	TERMINO_ALMOÇO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA;
+}
